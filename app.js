61b1513c698593e34be6d94f684bc26f64ac54b3
anookinov <anookinov@gmail.com>
2021-05-23 12:32:23 +0700

    add websocket

--- app.js
+++ app.js
@@ -77,6 +77,10 @@ mongoose
     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.yg0pj.mongodb.net/messages?retryWrites=true&w=majority`
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', (socket) => {
+      console.log('Client connected');
+    });
   })
   .catch((err) => console.log(err));
